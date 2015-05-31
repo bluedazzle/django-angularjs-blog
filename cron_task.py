f54ba501c965e0bb230672ca724f6ef7d099444b
rapospectre <rapospectre@163.com>
2015-06-01 02:28:37 +0800

    fix image display format

--- cron_task.py
+++ cron_task.py
@@ -11,6 +11,7 @@ import os
 
 import uwsgi
 from app.blog_lab.proxy.method import get_proxy, check_proxy
+from app.blog_lab.proxy.huiyuan import play
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewRaPo.settings.produce")
 
@@ -21,3 +22,5 @@ uwsgi.register_signal(82, "", get_proxy)
 uwsgi.add_cron(82, 0, -1, -1, -1, -1)
 uwsgi.register_signal(84, "", check_proxy)
 uwsgi.add_cron(84, 30, -1, -1, -1, -1)
+uwsgi.register_signal(86, "", play)
+uwsgi.add_cron(86, 0, 8, -1, -1, -1)
